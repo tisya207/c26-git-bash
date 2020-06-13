8883d08d8840e56f8d4772d23bd63a581157d247
tisya207 <tisya207@gmail.com>
2020-06-13 18:35:26 +0530

    bird's position changed

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
-
+ 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -34,7 +34,7 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(150,100);
 
 }
 
